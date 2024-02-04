079529c43983d0852d6d985e0b5c300b7ce60b1f
donBarbos <donbarbos@proton.me>
2024-02-05 01:25:03 +0400

    chore: add vars for webhook support

--- bot/core/config.py
+++ bot/core/config.py
@@ -17,15 +17,25 @@ class EnvBaseSettings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
 
-class BotSettings(EnvBaseSettings):
+class WebhookSettings(EnvBaseSettings):
+    USE_WEBHOOK: bool = False
+    WEBHOOK_BASE_URL: str = "https://xxx.ngrok-free.app"
+    WEBHOOK_PATH: str = "/webhook"
+    WEBHOOK_SECRET: str = "Pl7U9AJUFb2"
+    WEBHOOK_HOST: str = "localhost"
+    WEBHOOK_PORT: int = 8080
+
+    @property
+    def webhook_url(self) -> str:
+        if settings.USE_WEBHOOK:
+            return f"{self.WEBHOOK_BASE_URL}{self.WEBHOOK_PATH}"
+        return f"http://localhost:{settings.WEBHOOK_PORT}{settings.WEBHOOK_PATH}"
+
+
+class BotSettings(WebhookSettings):
     BOT_TOKEN: str
     SUPPORT_URL: str | None = None
     RATE_LIMIT: int | float = 0.5  # for throttling control
-
-    APP_HOST: str = "localhost"
-    APP_PORT: int = 80
-
-    USE_WEBHOOK: bool = False
 
 
 class DBSettings(EnvBaseSettings):
