e62bfe3f637ead73c67b52030a429c08de35edc7
donBarbos <donbarbos@proton.me>
2024-02-25 22:53:20 +0400

    fix(migrations): remove extra imports from models

--- migrations/env.py
+++ migrations/env.py
@@ -7,7 +7,6 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from bot.core.config import settings
-from bot.database.models import *  # noqa: F403
 from bot.database.models import Base
 
 # this is the Alembic Config object, which provides
